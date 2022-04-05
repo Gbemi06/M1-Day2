73d2ce7935375fe6833d4cb457dd2f6202e720f9
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-04-05 20:16:55 +0300

    CRUD operations completed

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import uniqid from "uniqid";
 
 const authorsRouter = express.Router();
 
@@ -14,32 +15,70 @@ console.log(parentFolderPath);
 const authorsPath = join(parentFolderPath, "authors.json");
 console.log(authorsPath);
 
+// CREATE --> POST request on http://localhost:3001/authors/ (+ body)
+
 authorsRouter.post("/", (request, response) => {
   console.log("request successful", request.body);
+  const newAuthor = { ...request.body, createdAt: new Date(), id: uniqid() };
 
-  response.status(200).send();
+  const author = JSON.parse(fs.readFileSync(authorsPath));
+
+  console.log("posted successful", author);
+
+  author.push(newAuthor);
+
+  fs.writeFileSync(authorsPath, JSON.stringify(author));
+
+  response.status(200).send({ id: newAuthor.id });
 });
 
+// READ --> GET request on http://localhost:3001/authors/ (+ optional query parameters)
 authorsRouter.get("/", (request, response) => {
-  console.log("request successful", request.body);
+  const authors = JSON.parse(fs.readFileSync(authorsPath, "utf-8"));
 
-  response.send();
+  response.send(authors);
 });
 
-authorsRouter.get("/:authorsId", (request, response) => {
-  console.log("request successful", request.body);
+// READ (single Author) --> GET request on http://localhost:3001/authors/:authorId
 
-  response.send();
+authorsRouter.get("/:authorId", (request, response) => {
+  const authorId = request.params.authorId;
+  const authorsArray = JSON.parse(fs.readFileSync(authorsPath));
+
+  const getAuthor = authorsArray.find((author) => author.id === authorId);
+
+  response.send(getAuthor);
 });
 
-authorsRouter.put("/:authorsId", (request, response) => {
-  console.log("request successful", response.body);
+//  UPDATE (single author) --> UPDATE request on http://localhost:3001/authors/:authorId
 
-  response.send();
+authorsRouter.put("/:authorId", (request, response) => {
+  const authorId = request.params.authorId;
+  const authorsArray = JSON.parse(fs.readFileSync(authorsPath));
+
+  const authorIndex = authorsArray.find((author) => author.id === authorId);
+  const authorToEdit = authorsArray[authorIndex];
+  const editedAuthor = {
+    ...authorToEdit,
+    ...request.body,
+    updatedAt: new Date(),
+  };
+
+  authorsArray[authorIndex] = editedAuthor;
+
+  fs.writeFileSync(authorsPath, JSON.stringify(authorsArray));
+  response.send(editedAuthor);
 });
 
-authorsRouter.delete("/:authorsId", (request, response) => {
-  console.log("request successful", response.body);
+//  DELETE (single author) --> DELETE request on http://localhost:3001/authors/:authorId
+
+authorsRouter.delete("/:authorId", (request, response) => {
+  const authorId = request.params.authorId;
+
+  const authorsArray = JSON.parse(fs.readFileSync(authorsPath));
+  const otherAuthors = authorsArray.filter((author) => author.id !== authorId);
+
+  fs.writeFileSync(authorsPath, JSON.stringify(otherAuthors));
 
   response.send();
 });
